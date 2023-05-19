e79765fc371eeb26283d73fcbd4b83122bd5a6a3
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-05-19 19:26:12 +0530

    Added :switch case

--- src/com/bridglabz/EmployeeWageProblem/EmployeeWageComputationProblem.java
+++ src/com/bridglabz/EmployeeWageProblem/EmployeeWageComputationProblem.java
@@ -1,22 +1,24 @@
 package com.bridglabz.EmployeeWageProblem;
 
 public class EmployeeWageComputationProblem {
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
     public static void main(String[] args) {
-        System.out.println("Welcome to employee wage computation program");
-        isEmployee();
-    }
-    public static void isEmployee() {
-        int IS_PART_TIME=1;
-        int IS_FULL_TIME = 2;
-        int EMP_RATE_PER_HOUR=20;
-        int empHour=0;
-        int empWage=0;
+        int empHour = 0;
+        int empWage = 0;
         double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_FULL_TIME)
-            empHour = 8;
-            else if(empCheck==IS_PART_TIME)
-            empHour=4;
-           empWage=empHour * EMP_RATE_PER_HOUR;
-            System.out.println("Emp wage:"+empWage);
-        }
+        switch((int)empCheck){
+            case IS_FULL_TIME:
+                empHour=8;
+                break;
+            case IS_PART_TIME:
+                empHour=4;
+                break;
+            default:
+                empHour=0;
     }
+        empWage=empHour*EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage: "+empWage);
+    }
+}

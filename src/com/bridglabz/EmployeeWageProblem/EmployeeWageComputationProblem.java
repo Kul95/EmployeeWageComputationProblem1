b2583608f9e2d9474bbd5659ba9ba40e1bc245bc
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-05-19 19:07:31 +0530

    Added : Part time employee wage

--- src/com/bridglabz/EmployeeWageProblem/EmployeeWageComputationProblem.java
+++ src/com/bridglabz/EmployeeWageProblem/EmployeeWageComputationProblem.java
@@ -6,15 +6,17 @@ public class EmployeeWageComputationProblem {
         isEmployee();
     }
     public static void isEmployee() {
-        int IS_FULL_TIME = 1;
+        int IS_PART_TIME=1;
+        int IS_FULL_TIME = 2;
         int EMP_RATE_PER_HOUR=20;
         int empHour=0;
         int empWage=0;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_FULL_TIME) {
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == IS_FULL_TIME)
             empHour = 8;
+            else if(empCheck==IS_PART_TIME)
+            empHour=4;
            empWage=empHour * EMP_RATE_PER_HOUR;
             System.out.println("Emp wage:"+empWage);
         }
     }
-}

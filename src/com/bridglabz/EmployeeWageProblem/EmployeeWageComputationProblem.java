17151524bd8c034d7d68950c250d312f16a78f52
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-05-19 18:58:02 +0530

    Daily employee wage

--- src/com/bridglabz/EmployeeWageProblem/EmployeeWageComputationProblem.java
+++ src/com/bridglabz/EmployeeWageProblem/EmployeeWageComputationProblem.java
@@ -3,5 +3,18 @@ package com.bridglabz.EmployeeWageProblem;
 public class EmployeeWageComputationProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation program");
+        isEmployee();
+    }
+    public static void isEmployee() {
+        int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR=20;
+        int empHour=0;
+        int empWage=0;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME) {
+            empHour = 8;
+           empWage=empHour * EMP_RATE_PER_HOUR;
+            System.out.println("Emp wage:"+empWage);
+        }
     }
 }

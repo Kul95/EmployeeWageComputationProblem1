420b5673a58190b97ed22c8299d376a0cfb59487
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-05-19 18:24:00 +0530

    Welcome to employee wage problem

---
+++ src/com/bridglabz/EmployeeWageProblem/EmployeeWageComputationProblem.java
@@ -0,0 +1,7 @@
+package com.bridglabz.EmployeeWageProblem;
+
+public class EmployeeWageComputationProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage computation program");
+    }
+}
